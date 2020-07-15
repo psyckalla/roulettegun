04b2518dc54cca045fa8ed7790be26ee5d8bb772
Katerina <psyckalla.gmail.com>
2020-07-15 15:43:41 -0400

    commit

--- src/RouletteGun.js
+++ src/RouletteGun.js
@@ -1,26 +1,43 @@
 import React from 'react';
 
 class RouletteGun extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { chamber: null,
-            spinningTheChamber: false
+    constructor (props) {
+        super (props);
+        this.state = {
+            chamber: null,
+            spinningTheChamber: false,
+            default: 'spinning the chamber and pulling the trigger! ...',
+            renderBang: 'BANG!!!!',
+            renderSafe: "you're safe"
         }
     }
 
-    clickEventHandler() {
-        this.setState = () => {
-            return true
+    bulletInChamber() {
+        return Math.ceil(Math.random() * 8);
+    }
+
+    handleEventClick() {
+        this.setState({chamber: this.bulletInChamber()})
+        console.log(this.state.chamber)
+    }
+
+    renderRouletteGun() {
+
+        if (this.state.chamber === this.bulletInChamber) {
+            return console.log(this.state.renderBang)
+        } else if (this.state.chamber !== this.bulletInChamber) {
+             return console.log(this.state.renderSafe, this.state.chamber, this.bulletInChamber)
         }
     }
 
-    render() {
-        return(
+    render() { 
+        return (
             <div>
-                <p>spinning the chamber and pulling the trigger! ...</p>
-                <input onClick={() => this.clickEventHandler()} type="button" value="Pull the trigger!" />
+                <p>{this.renderRouletteGun()}</p>
+                <input onClick={(e) => this.handleEventClick()} type="button" value="Pull the trigger!" />
             </div>
         )
+
     }
 }
 
